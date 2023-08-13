1c12c35a9a2ab07d31d878549f646d8f5f18e3c2
nedimmujcinovic <nedimmujcinovic1@gmail.com>
2023-08-13 12:18:43 +0200

    Changes

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
+<html lang="en">
+    <head>
+        <title>Cars</title>
+    </head>
+    <body>
+        <form method="GET">
+         <select name="car">
+             <option value="ALL">ALL</option>
+            <option value="BMW">BMW</option>
+            <option value="Audi">Audi</option>
+        </select>
+        <button type="submit">Submit</button>
+    </form>
+    </body>
+
+    </html> 
+
 <?php
 
-echo "Hello my first app "\n" is loaded index.php";
+isset($_GET)
+
+$cars = [
+    "BMW" => ["M3", "M5", "X4M"],
+    "Audi" => ["A3", "A6", "A8"]
+];
+foreach($cars as $car => $models ) {
+    echo "<h2> $car </h2>";
+    foreach($models as $c){
+        echo "<i> $c </i> <br>";
+    }
+}
 
 ?>
